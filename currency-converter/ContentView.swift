8898b1a4b08871e36d05f62d1c952bf7824fffc9
hzq886 <hzq886@gmail.com>
2026-03-19 21:54:50 +0900

    Initial Commit

---
+++ currency-converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  currency-converter
+//
+//  Created by hzq on 2026/03/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ currency-converter/currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converterApp.swift
+//  currency-converter
+//
+//  Created by hzq on 2026/03/19.
+//
+
+import SwiftUI
+
+@main
+struct currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
